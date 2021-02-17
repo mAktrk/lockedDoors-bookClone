c8bd2ad63d32b063e59f99ce3e3b7d03c66ccd5d
Mücahit Aktürk <m.akturk6134@gmail.com>
2021-02-17 14:15:23 +0300

    Project

--- Locked Doors/Assets/Script/PlayerScript.cs
+++ Locked Doors/Assets/Script/PlayerScript.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private int skor = 0;
+    public Text toplamSkor;
+    [SerializeField] private GameObject anahtaVar;
+
+    [SerializeField] private float hiz;
+    private bool sagaBak;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sagaBak = true;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        float yatay = Input.GetAxisRaw("Horizontal");
+        KarakterHaraket(yatay);
+        YonCevir(yatay);
+
+    }
+
+    private void KarakterHaraket(float yatay)
+    {
+        rb.velocity = new Vector2(yatay * hiz, rb.velocity.y);
+        animator.SetFloat("karakterHizi", Mathf.Abs(yatay));
+    }
+
+    private void YonCevir(float yatay)
+    {
+        if (yatay > 0 && !sagaBak || yatay < 0 && sagaBak)
+        {
+            sagaBak = !sagaBak;
+            Vector3 yon = transform.localScale;
+            yon.x *= -1;
+            transform.localScale = yon;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag=="altin")
+        {
+            other.gameObject.SetActive(false);
+            skor = skor + 100;
+            SkorAyarla();
+        }
+        if (other.gameObject.tag == "anahtar") 
+        {
+            other.gameObject.SetActive(false);
+            anahtaVar.SetActive(true);
+        }
+    }
+
+
+    private void SkorAyarla()
+    {
+        toplamSkor.text = "SKOR:"+skor;
     }
 }
